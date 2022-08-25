d7281c9967046c134fb1febf928d41c11f1a6a51
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-08-24 18:08:51 -0700

    second attempt, debugged
    the first version of the program is close, but its not quite yielding the right results, getting "hELlo" instead of "hElLo"
    
    Second attempt works

--- jsexercises/JSI-exercises/capitalize-even-letters.js
+++ jsexercises/JSI-exercises/capitalize-even-letters.js
@@ -1,28 +1,45 @@
-//Write a code that takes in a string and then prints
+//Write code that takes in a string and then prints
 // back with even letters upcased and the rest downcased. 
 //For example, "hello" becomes "hElLo"
 
-// create the string variable
-// iterate through the string to determine the even letters
-//capitalize only that and move to the next one
 
-
-
-//its close but I can't quick get it to do every second letter
+//its close but I can't quite get it to do every second letter
 //it seems to do two in a row, I think its the i++ placement
+
+//should print: hElLo
 
 let theString = prompt("enter a word or sentence");
 theString = theString.toLowerCase();
 let i = 0;
-let x=(i%2)
+
 
 while (i < theString.length){
     if (i % 2 === 1){
         let currentLetter = theString[i];
         let newLetter = theString[i].toUpperCase();
         theString = theString.replace(currentLetter, newLetter);
-    } i++
-console.log(i,theString[i], x)
+    } 
+i++
 }
 
 console.log(theString);
+
+// attempt 2:
+
+let theString = prompt("enter a word or sentence");
+//theString = theString.toLowerCase();
+let i = 0;
+let returnString = "";
+
+while (i < theString.length){
+
+    if (i % 2 == 1) {
+        returnString +=theString[i].toUpperCase()
+    } else if (i % 2 == 0) {
+        returnString += theString[i].toLowerCase()
+    }
+    i++
+} 
+
+console.log(returnString)
+
